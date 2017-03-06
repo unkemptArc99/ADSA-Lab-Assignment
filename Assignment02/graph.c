d04843bcbfb477d75a67b6744ccce7e05eb425ef
unkemptArc99 <abhishekbhardwaj540@gmail.com>
2017-03-06 19:58:03 +0000

    Assignment02 : Added all the other graphs

--- Assignment02/graph.c
+++ Assignment02/graph.c
@@ -4,7 +4,7 @@ int main()
 	
 	FILE * gnuplotPipe = popen ("gnuplot -persistent", "w");
 	
-	char * commandsForGnuplot[] = {"set terminal png","set output 'desc_ibsr.png'","set xlabel 'Input Size'","set ylabel 'Time taken (in s)'","plot 'output_ins_desc.txt' using 1:2 with lines title 'Insertion','output_bub_desc.txt' using 1:2 with lines title 'Bubble', 'output_rank_desc.txt' using 1:2 with lines title 'Rank', 'output_sel_desc.txt' using 1:2 with lines title 'Selection'"};
+	char * commandsForGnuplot[] = {"set terminal png","set output 'desc_mq.png'","set xlabel 'Input Size'","set ylabel 'Time taken (in s)'","plot 'output_qui_desc.txt' using 1:2 with lines title 'Quick','output_mer_desc.txt' using 1:2 with lines title 'Merge'"};
 	for (int i=0; i <5; i++)
     	{
     	fprintf(gnuplotPipe, "%s \n", commandsForGnuplot[i]); //Send commands to gnuplot one by one.
