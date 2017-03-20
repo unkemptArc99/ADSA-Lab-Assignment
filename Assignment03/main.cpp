7c82cceec368cd0d293cc53e8a7c6a795a5812f9
unkemptArc99 <abhishekbhardwaj540@gmail.com>
2017-03-20 17:21:37 +0000

    Assignment03 : Made the final question program. Driver programs are remaining.

--- Assignment03/main.cpp
+++ Assignment03/main.cpp
@@ -1,53 +1,85 @@
 /****************************************************************
 Name : Abhishek
 Roll No : B15103
-CS202 - DSA - Assignment03 - Main question file
+CS202 - DSA - Assignment03 - Main question file - Question 1 - Post fix
 ****************************************************************/
 
 #include <iostream>
 #include <cstdlib>
 #include <string>
-#include <algorithm>
+#include <cmath>
 #include "list.hpp"
 #include "stack.hpp"
 
 using namespace std;
 
+int evaluator(string a);							//Main postfix evaluation function
+int performOp(char operand, int op1, int op2);		//Performing operations on op1 and op2 using the operand
+bool isOp(char c);									//To verify if c is operand or not
+bool isNum(char c);									//To verify if c is numeric digit or not
+
 int main(int argc, char const *argv[])
 {
 	cout<<"Enter your input string : ";
 	string a;
-	cin>>a;
-	vector<int> s;
-	for (int i = 0; i < a.length(); ++i)
-	{
-		
-	}
-	reverse(s.begin(),s.end());
-	cs202::stack<int> sta;
-	for(int i = 0; i < s.length(); ++i){
-		if(s[i] >= 48 && s[i] <= 57){
-			sta.push(s[i]-48);
-		}
-		else{
-			int x1,x2;
-			x1 = sta.pop();
-			x2 = sta.pop();
-			if(s[i]=='+'){
-				sta.push(x1+x2);
-			}
-			else if(s[i]=='-'){
-				sta.push(x1-x2);
-			}
-			else if(s[i]=='*'){
-				sta.push(x1*x2);
-			}
-			else if(s[i]=='/'){
-				sta.push(x1/x2);
-			}
-		}
-	}
-	int x = sta.pop();
-	cout<<x<<endl;
+	getline(cin,a);
+	int result = evaluator(a);
+	cout<<"Output = "<<result<<endl;
 	return 0;
+}
+
+int evaluator(string a){
+	cs202::stack<int> s;
+	for (int i = a.length()-1; i >= 0; i--)
+	{
+		if(a[i]==' ' || a[i]=='(' || a[i]==')')
+			continue;
+		else if(isOp(a[i])){
+			if(!s.empty()){
+				int op1 = s.pop();
+				int op2 = s.pop();
+				int result = performOp(a[i],op1,op2);
+				s.push(result);
+			}
+		}
+		else if(isNum(a[i])){
+			int op = 0;
+			int key = i;
+			while(i>=0 && isNum(a[i])){
+				op += (a[i]-'0')*((int)pow(10,key-i));
+				i--;
+			}
+			i++;
+			s.push(op);
+		}
+	}
+	return s.top();
+}
+
+int performOp(char operand, int op1, int op2){
+	if(operand == '+')
+		return op1+op2;
+	else if(operand == '-')
+		return op1-op2;
+	else if(operand == '*')
+		return op1*op2;
+	else if(operand == '/')
+		return op1/op2;
+	else
+		cout<<"ERROR!!"<<endl;
+	return -1;
+}
+
+bool isOp(char c){
+	if(c == '+' || c == '-' || c == '*' || c == '/')
+		return true;
+	else
+		return false;
+}
+
+bool isNum(char c){
+	if(c >= '0' && c <= '9')
+		return true;
+	else
+		return false;
 }
