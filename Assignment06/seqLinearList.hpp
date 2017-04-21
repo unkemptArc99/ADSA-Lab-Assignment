c8b339c1945b21c74c5af74b7157adf771ac1e8c
unkemptArc99 <abhishekbhardwaj540@gmail.com>
2017-04-21 13:45:05 +0000

    Assignment06 : Added the Linear List header file for the assignment. Working on the minPriorityQueue header file

---
+++ Assignment06/seqLinearList.hpp
@@ -0,0 +1,272 @@
+#ifndef _SEQ_LINEAR_LIST
+#define _SEQ_LINEAR_LIST 1
+
+#include <iostream>
+#include <cstdlib>
+#include <string.h>
+#include <exception>
+
+namespace cs202 {
+	template<class T>
+	class LinearList{
+		private:
+        	T* buffer_;
+        	unsigned int size_;
+        	unsigned int capacity_;
+      	public:
+        // Default Constructor
+        // Creates a zero sized LinearList
+        	LinearList();
+
+        // Constructor with size as input
+        	LinearList(const int& isize);                   
+
+        // Constuctor with size and default value as input
+        	LinearList(const int& isize, const T& ival);    
+
+        // Default Destructor
+        	~LinearList();                                  
+
+        // Accessing an element of the LinearList using operator
+        	inline T& operator[](const int& i){
+        		return buffer_[i];
+        	};         
+
+        // Return the element in the list at position k
+        	inline T& at(const int& k){
+        		return buffer_[k];
+        	};         
+
+        // Appending an element to the LinearList
+        // The asymptotic time complexity of this function
+        // should be O(1)
+        	void push_back(const T& item);              
+
+        // Returns true if LinearList is empty
+        	bool empty();                               
+
+        // Returns the size of the LinearList
+        	unsigned int size(){
+        		return size_;
+        	};                                 
+
+        // Returns the capacity of the LinearList
+        	unsigned int capacity(){
+        		return capacity_;
+        	};                                 
+
+        // Fills all the elements with a default value
+        	void fill(const T& item);                   
+
+        // Resizes the LinearList
+        	void resize(const int& n); 
+
+        // Find the index of first occurence of an item in the list
+        // Return size of list if item not found
+        // 0-based indexing
+        	int find(const T& item);
+
+        // Remove all occurences of an item in the list
+        	void erase(const T& item);
+
+        // Remove the item in the list at position pos
+        	void erase_pos(const int& pos);
+
+        // Insert item at position k
+        	void insert(const T& item, const int& k);
+
+        // Iterator class for LinearList
+        // This should be used to traverse the LinearList
+        // See std::vector<T>::iterator for more understanding
+        // on how iterators work
+        	class iterator{                             
+          		private:
+            		T* elem_;
+          		public:
+            		iterator();
+            		iterator(T* ptr);
+            		~iterator();
+            		T& operator*();
+            		void operator=(T *ptr);
+            		void operator=(iterator iter);
+            		void operator++();
+            		void operator--();
+            		bool operator!=(T* ptr);
+            		bool operator==(T* ptr);
+            		bool operator!=(iterator iter);
+            		bool operator==(iterator iter);
+            		iterator operator+(int i);
+            		iterator operator-(int i);
+        	};
+        	iterator begin();                       
+        	iterator end();
+  	};
+
+  	template<class T>
+  	LinearList<T>::LinearList(void){
+  		buffer_=(T *)NULL;
+  		capacity_=0;
+  		size_=0;
+	}
+
+	template<class T>
+	LinearList<T>::LinearList(const int& isize){
+		capacity_=isize;
+		size_=0;
+		buffer_=(T *)malloc(capacity_*sizeof(T));
+		for(int i=0;i<capacity_;++i)
+			buffer_[i]=(T)NULL;
+	}
+
+	template<class T>
+	LinearList<T>::LinearList(const int& isize, const T& ival){
+		capacity_=isize;
+		size_=capacity_;
+		buffer_=(T *)malloc(capacity_*sizeof(T));
+		for(int i=0;i<capacity_;++i)
+			buffer_[i]=ival;
+	}
+
+	template<class T>
+	LinearList<T>::~LinearList(void){
+		free(buffer_);
+	}
+
+	template<class T>
+	void LinearList<T>::push_back(const T& item)
+	{
+		if(size_>=capacity_){
+			T* some_ptr = (T*) realloc(buffer_, (capacity_+1)*sizeof(T));
+			if(some_ptr != NULL){
+				buffer_ = some_ptr;
+				buffer_[size_] = item;
+				size_++;
+			}
+			else{
+				throw -1;
+			}
+		}
+		else{
+			buffer_[size_]=item;
+			size_++;
+		}
+	}
+
+	template<class T>
+	bool LinearList<T>::empty(){
+		if(size_==0)
+			return true;
+		else
+			return false;
+	}
+
+	template<class T>
+	void LinearList<T>::fill(const T& item){
+		for (int i = 0; i < capacity_; ++i)
+		{
+			buffer_[i]=item;
+		}
+	}
+
+	template<class T>
+	void LinearList<T>::resize(const int& n){
+		buffer_=(T *)realloc(buffer_,n*sizeof(T));
+		capacity_=n;
+		if(n<size_)
+			size_=n;
+	}
+
+	template<class T>
+	int LinearList<T>::find(const T& item){
+		int i;
+		for(i=0;i<size_;++i){
+			if(buffer_[i]==item){
+				break;
+			}
+		}
+		return i;
+	}
+
+	template<class T>
+	void LinearList<T>::erase(const T& item){
+		for (int i = 0; i < size_; ++i){
+			if(buffer_[i]==item){
+				for (int j = i; j < size_-1; ++i){
+					buffer_[i]=buffer_[i+1];
+				}
+				buffer_[size_-1]=(T)NULL;
+				size_--;
+			}
+		}
+	}
+
+	template<class T>
+	void LinearList<T>::erase_pos(const int& pos){
+		if(pos >= size_){
+			throw -1;
+		}
+		else{
+			for(int i = pos;i < size_-1; ++i){
+				buffer_[i] = buffer_[i+1];
+			}
+			buffer_[size_-1]=(T)NULL;
+			size_--;
+		}
+	}
+
+	template<class T>
+	void LinearList<T>::insert(const T& item, const int& k){
+		if(size_>=capacity_){
+			T* some_ptr = (T*) realloc(buffer_, (capacity_+1)*sizeof(T));
+			if(some_ptr != NULL){
+				buffer_ = some_ptr;
+				buffer_[k] = item;
+				size_++;
+			}
+			else{
+				throw -1;
+			}
+		}
+		else if(size_ == 0){
+			size_++;
+			if(k < capacity_)
+				buffer_[k]=item;
+			else{
+				T* some_ptr = (T*) realloc(buffer_, (k + 1)*sizeof(T));
+				if(some_ptr != NULL){
+					buffer_ = some_ptr;
+					buffer_[k] = item;
+				}
+				else{
+					throw -1;
+				}
+			}
+		}
+		else{
+			size_++;
+			if(k < capacity_){
+				for(int i=size_-1;i>k;i--){
+					buffer_[i]=buffer_[i-1];
+				}
+				buffer_[k]=item;
+			}
+			else{
+				T* some_ptr = (T*) realloc(buffer_, (k + 1)*sizeof(T));
+				if(some_ptr != NULL){
+					buffer_ = some_ptr;
+					for(int i=size_-1;i>k;i--){
+						buffer_[i]=buffer_[i-1];
+					}
+					buffer_[k] = item;
+				}
+				else{
+					throw -1;
+				}
+			}
+		}
+	}
+
+}
+
+#endif  /* _SEQ_LINEAR_LIST */
+
