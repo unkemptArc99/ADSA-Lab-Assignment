021dadb466b3d7cfa9b484dacac4bca97f4361d3
unkemptArc99 <abhishekbhardwaj540@gmail.com>
2017-04-21 14:50:45 +0000

    Assignment06 : Added the private functions in the minPriorityQueue header file. All other functions are in progress

--- Assignment06/minPriorityQueue.hpp
+++ Assignment06/minPriorityQueue.hpp
@@ -13,7 +13,7 @@ ADSA Assignment06 - Minimum Priority Queue Header File
 
 #include "seqLinearList.hpp"
 
- namespace cs202{
+namespace cs202{
  	
  	template<typename T>
  	class MinPriorityQueue{
@@ -32,55 +32,101 @@ ADSA Assignment06 - Minimum Priority Queue Header File
 
  		// heapifies the heap at position pos 
  		void heapify(const size_t& pos){
+            int left = left_child(pos);
+            int right = right_child(pos);
+            int smallest;
+            
+            if(left <= heap_size && heap[left] < heap[pos]){
+                smallest = left;
+            }
+            else{
+                smallest = pos;
+            }
 
-         }
+            if(right <= heap_size && heap[right] < heap[smallest]){
+                smallest = right;
+            }
+            if(smallest != pos){
+                heap.swapper(pos,smallest);
+                heapify(smallest);
+            }
+        }
 
  		// return the parent of an element
- 		inline int parent(const size_t& pos);
+ 		inline int parent(const size_t& pos){
+            return pos/2;
+        }
 
  		// return the left child of an element
- 		inline int left_child(const size_t& pos);
+ 		inline int left_child(const size_t& pos){
+            return 2*pos;
+        }
 
  		// return the right child of an element
- 		inline int right_child(const size_t& pos);
+ 		inline int right_child(const size_t& pos){
+            return (2*pos + 1);
+        }
 
  	public:
  		// default constructor
  		MinPriorityQueue(){
-			 heap(100);
+			 heap(101);
              heap_size = 0;
+             heap.push_back(std::numeric_limits<T>::max());
          }
 
  		// construct a heap from the elements of a LinearList
  		MinPriorityQueue(const LinearList<T>& v){
-			heap.resize(v.capacity());
-			for(int i = 0; i < capacity_; i++){
-				heap.push_back(v[i]);
+			heap.resize(v.capacity() + 1);
+            heap.push_back(std::numeric_limits<T>::max());
+			for(int i = 1; i < v.capacity() + 1; i++){
+				heap.push_back(v[i - 1]);
 			}
+            heap_size = 0;
 		}
 
  		// insert a value to the heap
  		void insert(const T& a){
-			heap.push_back(a);
+			
         }
 
  		// get the minimum element from the heap
  		inline T minimum(){
-			 int min = std::numeric_limits<T>::max();
-			 for(int i = 0; i < heap.capacity(); i++){
-				 
-			 }
-		 }
+		    int minPriority = std::numeric_limits<T>::max();
+			for(int i = 0; i < heap_size; i++){
+                if(minPriority > heap[i]){
+                    minPriority = heap[i];
+                }
+			}
+            return minPriority;
+		}
 
  		// return the minimum element from the heap and remove it
  		// as well
- 		T extract_min();
+ 		T extract_min(){
+            int minPriority = std::numeric_limits<T>::max();
+            int index = 0;
+			for(int i = 0; i < heap_size; i++){
+                if(minPriority > heap[i]){
+                    minPriority = heap[i];
+                    index = i;
+                }
+			}
+            heap.erase_pos(index);
+            heap.push_back(minPriority);
+            heap_size--;
+            return minPriority;
+        }
 
  		// return the heap size
- 		inline int heap_size();
+ 		inline int get_heap_size(){
+            return heap_size;
+        }
 
-	// build a heap from elements of a LinearList container 
- 		void build_heap(const LinearList<T>& v);
+	    // build a heap from elements of a LinearList container 
+ 		void build_heap(const LinearList<T>& v){
+             
+         }
 
  		// check if MinPriorityQueue is empty
  		inline bool empty();
@@ -88,5 +134,5 @@ ADSA Assignment06 - Minimum Priority Queue Header File
  		// heap_decrease_key function
  		void heap_decrease_key(const size_t& pos);
  	};
- 	
+}	
 #endif 
