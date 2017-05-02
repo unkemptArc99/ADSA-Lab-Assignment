9a200bd0e884fe6075b1fde700799ad1927e92cc
unkemptArc99 <abhishekbhardwaj540@gmail.com>
2017-05-02 13:34:38 +0000

    Assignment07 : Added blocked cell input taker in FFD

--- Assignment07/FloodFilldriver.cpp
+++ Assignment07/FloodFilldriver.cpp
@@ -19,8 +19,6 @@ int main(){
     char rep;
     //Variable to store the size of matrix,source and destination cell
     int n,m,source,dest;
-    //List to store the non-visiting cells;
-    cs202::LinearList<int> X;
 
     cout<<endl<<"Enter the value of n (number of columns of matrix OR the maximum X-axis value of the matrix) : ";
     cin>>n;
@@ -101,6 +99,83 @@ int main(){
         }
     }
 
-    
+    //taking the blocked cells
+    cout<<endl<<"Enter the number of blocked cells : ";
+    int num;
+    cin>>num;
+    cout<<endl;
+    for(int i = 0; i < num; ++i){
+        int x,y;
+        cout<<endl<<"Point "<<i + 1<<endl;
+        cout<<"Enter x-coordinate (0-based) : ";
+        cin>>x;
+        cout<<"Enter y-coordinate (0-based) : ";
+        cin>>y;
+        int cell = x*n + y;
+        int temp = i;
+        i = cell;
+        if(i == 0){
+            A->remove(i, i + 1);
+            A->remove(i, i + n);
+            A->remove(i, i + n + 1);
+        }
+        else if(i == n - 1){
+            A->remove(i, i - 1);
+            A->remove(i, i + n);
+            A->remove(i, i + n - 1);
+        }
+        else if(i == n * (m - 1)){
+            A->remove(i, i + 1);
+            A->remove(i, i - n);
+            A->remove(i, i - n + 1);
+        }
+        else if(i == n * m - 1){
+            A->remove(i, i - 1);
+            A->remove(i, i - n);
+            A->remove(i, i - n - 1);
+        }
+        else if(i % n == 0){
+            A->remove(i, i + 1);
+            A->remove(i, i + n);
+            A->remove(i, i - n);
+            A->remove(i, i + n + 1);
+            A->remove(i, i - n + 1);
+        }
+        else if(i % n == n - 1){
+            A->remove(i, i - 1);
+            A->remove(i, i + n);
+            A->remove(i, i - n);
+            A->remove(i, i + n - 1);
+            A->remove(i, i - n - 1); 
+        }
+        else if(i / n == 0){
+            A->remove(i, i + 1);
+            A->remove(i, i - 1);
+            A->remove(i, i + n);
+            A->remove(i, i + n + 1);
+            A->remove(i, i + n - 1);
+        }
+        else if(i / n == m - 1){
+            A->remove(i, i + 1);
+            A->remove(i, i - 1);
+            A->remove(i, i - n);
+            A->remove(i, i - n + 1);
+            A->remove(i, i - n - 1);
+        }
+        else{
+            //lateral
+            A->remove(i, i + 1);
+            A->remove(i, i - 1);
+            A->remove(i, i + n);
+            A->remove(i, i - n);
+            //diagonals
+            A->remove(i, i + n + 1);
+            A->remove(i, i + n - 1);
+            A->remove(i, i - n + 1);
+            A->remove(i, i - n - 1);
+        }
+        i = temp;
+    }
+
     return 0;
 }
