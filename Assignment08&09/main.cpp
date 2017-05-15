9dd51f9c4e72a02f24bba4c7a79784685fc0d762
unkemptArc99 <abhishekbhardwaj540@gmail.com>
2017-05-15 17:05:03 +0000

    Assignment0809 : Completed the writing of main

--- Assignment08&09/main.cpp
+++ Assignment08&09/main.cpp
@@ -7,13 +7,50 @@
 
 using namespace std;
 
+//n will store the vertices and m will store the edges
+int n,m;
+
+cs202::LinearList<int> sou(m);
+cs202::LinearList<int> des(m);
+
+void storing(int& a,int& b) {
+    sou.push_back(a);
+    des.push_back(b);
+}
+
+//handles the questions
+void answering(ifstream &in) {
+    int q;
+    in >> q;
+
+    for(int i = 0; i < q; ++i) {
+        int x,y;
+        in >> x >> y;
+        bool flag = false;
+        for (int j = 0; j < sou.size() && !flag; ++j) {
+            if(x == sou.at(i) && y == des.at(i)) { 
+                flag = true;
+            }
+            else if(y == sou.at(i) && x == des.at(i)) {
+                flag = true;
+            }
+        }
+        if(flag) {
+            cout<<"Yes"<<endl;
+        }
+        else {
+            cout<<"No"<<endl;
+        }
+    }
+}
+
 int main() {
     //taking the input of the file name
     cout<<"Enter the name of the file : ";
     string s;
     cin>>s;
     //file pointer to handle the input file
-    ifstream in(s);
+    ifstream in(s.c_str());
 
     //if input file named file doesn't exist report error
     if(!in) {
@@ -21,8 +58,6 @@ int main() {
         return -1;
     }
 
-    //n will store the vertices and m will store the edges
-    int n,m;
     in>>n>>m;
 
     //The main undirected graph for the grid
@@ -32,8 +67,11 @@ int main() {
     for(int i = 0; i < m; ++i) {
         int x,y,c;
         in>>x>>y>>c;
-        
+        A.add(x,y,c);
     }
 
+    A.kruskal(storing);
+
+    answering(in);
     return 0;
 }
